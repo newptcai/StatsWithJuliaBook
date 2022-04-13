afbe3320238be279a2337d59365f33ddd4015426
Xing Shi Cai <newptcai@gmail.com>
2022-04-13 23:10:37 +0800

    fix DataFrame problem

--- 4_chapter/dataframeImputation.jl
+++ 4_chapter/dataframeImputation.jl
@@ -1,5 +1,5 @@
 using DataFrames, CSV, Statistics
-data = CSV.read("./data/purchaseData.csv", DataFrame)
+data = CSV.read("data/purchaseData.csv", DataFrame)
 
 rowsKeep = .!(ismissing.(data.Grade) .& ismissing.(data.Price))
 data = data[rowsKeep, :]
@@ -7,8 +7,8 @@ data = data[rowsKeep, :]
 replace!(x -> ismissing(x) ? "QQ" : x, data.Name)
 replace!(x -> ismissing(x) ? "31/06/2008" : x, data.Date)
 
-grPr = combine(dropmissing(data), :Grade, :Price=>x -> 
-	AvgPrice = round(mean(x), digits=-3))
+grPr = combine(groupby(dropmissing(data), :Grade), 
+    :Price=> (x -> round(mean(x), digits=-3)) => :AvgPrice)
 
 d = Dict(grPr[:,1] .=> grPr[:,2])
 nearIndx(v, x) = findmin(abs.(v.-x))[2]
