3e021eaff4e588b1176b106acdd359ee4b8f74e7
Xing Shi Cai <newptcai@gmail.com>
2022-11-17 17:44:16 +0800

    fix marginalHists.jl

--- 4_chapter/marginalHists.jl
+++ 4_chapter/marginalHists.jl
@@ -1,12 +1,12 @@
-using StatsPlots, Distributions, CSV, DataFrames, Measures; pyplot()
+using Tables, StatsPlots, Distributions, CSV, DataFrames, Measures; gr()
 
-realData = CSV.read("../data/temperatures.csv")
+realData = CSV.read("../data/temperatures.csv", DataFrame)
 
 N = 10^5
 include("../data/mvParams.jl")
 biNorm = MvNormal(meanVect,covMat)
-syntheticData = DataFrame(rand(MvNormal(meanVect,covMat),N)')
-rename!(syntheticData,[:x1=>:Brisbane, :x2 => :GoldCoast])
+syntheticData = DataFrame(Tables.table(rand(MvNormal(meanVect,covMat),N)'))
+rename!(syntheticData,[:Column1=>:Brisbane, :Column2 => :GoldCoast])
 
 default(c=cgrad([:blue, :red]), 
     xlabel="Brisbane Temperature", 
